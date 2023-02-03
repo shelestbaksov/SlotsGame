3890a917ecdc9b66cccb110c9b13da52799c3aa5
Leisan Latypova <latley90@gmail.com>
2023-02-03 12:04:06 +0300

    Initial Commit

---
+++ SlotsGame/SlotsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsGameApp.swift
+//  SlotsGame
+//
+//  Created by Leysan Latypova on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlotsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
