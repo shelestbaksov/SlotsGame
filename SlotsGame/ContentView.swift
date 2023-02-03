a360e6ab2c66a8b3a2dcfd69938abe3536db1d0c
Leisan Latypova <latley90@gmail.com>
2023-02-03 12:26:48 +0300

    Initial commit

--- SlotsGame/ContentView.swift
+++ SlotsGame/ContentView.swift
@@ -8,12 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var creditScore = 0
+    @State private var firstSlot = "apple"
+    @State private var secondSlot = "apple"
+    @State private var thirdSlot = "apple"
+    
+    private var array = ["apple", "cherry", "star"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Text("SwiftUI Slots!").font(.largeTitle)
+            Spacer()
+            Text("Credit:\(creditScore)").font(.headline)
+            Spacer()
+            HStack {
+                Spacer()
+                Image(firstSlot).resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Image(secondSlot).resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Image(thirdSlot).resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+            Spacer()
+            Button {
+                firstSlot = array.randomElement() ?? "apple"
+                secondSlot = array.randomElement() ?? "apple"
+                thirdSlot = array.randomElement() ?? "apple"
+                
+                if firstSlot == secondSlot && secondSlot == thirdSlot {
+                    creditScore += 25
+                }
+            } label: {
+                Text("Spin").padding()
+                    .font(.system(size: 17, weight: .bold))
+            }
+            .padding([.leading, .trailing], 40)
+            .background(Color.pink)
+            .cornerRadius(25)
+            .foregroundColor(Color.white)
+            
+            Spacer()
+            
+
         }
         .padding()
     }
